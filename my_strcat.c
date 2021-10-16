b7060d15ce75c8f5794f308cefba2deb2ffbbe01
Nguyễn Văn Chung <91748834+Chung-Nguyen-05012002@users.noreply.github.com>
2021-10-16 12:00:14 +0700

    Add files via upload

---
+++ my_strcat.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+char *my_strcat(char *str1, char *str2)
+{
+    int len1, len2;
+    char *result;
+    len1 = strlen(str1);
+    len2 = strlen(str2);
+    result = (char *)malloc((len1 + len2 + 1)
+    * sizeof(char));
+    if(result == NULL)
+    {
+        printf("allocation failed!");
+        return NULL;
+    };
+    strcpy(result,str1);
+    strcpy(result + len1,str2);
+    return result;
+}
+int main ()
+{
+   char str1[100], str2[100];
+   char *cat_str;
+   printf("please enter two strings \n");
+   gets(str1);
+   fflush(stdin);
+   gets(str2);
+   cat_str = my_strcat(str1,str2);
+   if(cat_str == NULL)
+   {
+      printf("problem allocating memory !\n");
+      return 1;
+   };
+   printf("the concatenation of %s and %s is %s\n", 
+   str1, str2,cat_str);
+   free(cat_str);
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
